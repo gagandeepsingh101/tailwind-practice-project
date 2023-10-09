254fcb0891c4d39f678693b872abe49f523df80a
gagandeepsingh101 <gagandeepsingh128548@gmail.com>
2023-10-09 13:57:38 +0530

    Landing design responsive only for desktop

---
+++ Landing page create only for desktop/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwind').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    colors:{
+      darken:'hsl(193.55,93.94%,25.88%)',
+      whiteColor:'hsl(0,0%,100%)',
+      lightYellow:'hsl(44.42,79.38%,61.96%)',
+      lightRed:'hsl(14.36,98.32%,53.33%)',
+      darkpurple:'hsl(208.52,42.66%,28.04%)',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
